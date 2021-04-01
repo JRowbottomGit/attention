8cb77709042fe249ecf58d6d955289f5d826fb68
James Rowbottom <jrowbottom@twitter.com>
2021-04-01 16:50:58 +0100

    GAT tune 2

--- src/ray_tune.py
+++ src/ray_tune.py
@@ -271,11 +271,11 @@ def train_ray_int(opt, checkpoint_dir=None, data_dir="../data"):
 
 
 def set_cora_search_space(opt):
-    opt["model"] = "AGNN"#"GAT"
-    opt["att_type"] = tune.choice(["cosine","scaled_dot","pearson","spearman"]) #"GAT",
-    opt["num_layers"] = tune.choice([1,2,4,8,16])
+    opt["model"] = "GAT" #"AGNN"#"GAT"
+    opt["att_type"] = tune.choice(["GAT","cosine","scaled_dot","pearson","spearman"]) #,
+    opt["num_layers"] = tune.choice([1,2,4,8,10,12,14,16,20,24])
     opt['weight_decay'] = tune.loguniform(5e-5, 1e-3)  # weight decay l2 reg
-    opt['num_hidden'] = tune.sample_from(lambda _: 2 ** np.random.randint(4, 7))  # hidden dim of X in dX/dt
+    opt['num_hidden'] = tune.sample_from(lambda _: 2 ** np.random.randint(4, 8))  # hidden dim of X in dX/dt
     opt["lr"] = tune.loguniform(0.0001, 0.05)
     opt["in_drop"] = tune.uniform(0.4, 0.6)
     opt["optimizer"] = "adamax"
